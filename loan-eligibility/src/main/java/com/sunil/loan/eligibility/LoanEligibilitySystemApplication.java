d1878c0574f2b750b26214bb47e0899ea884cf9f
SunilReddy93 <sunilreddy814@gmail.com>
2026-03-14 11:41:11 +0530

    initial project structure

---
+++ loan-eligibility/src/main/java/com/sunil/loan/eligibility/LoanEligibilitySystemApplication.java
@@ -0,0 +1,13 @@
+package com.sunil.loan.eligibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEligibilitySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanEligibilitySystemApplication.class, args);
+    }
+
+}
